f6ac14e6f547fc25418e56b7fdecdb4e8b8701e5
Lam <LamDuong.inbox@gmail.com>
2021-08-17 11:54:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    const thankYou = [];
+
+    for (var i = 0; i < array.length; i++) {
+        thankYou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return thankYou;
+}
+
+console.log(writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise'));
+
+function countDown(num) {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
